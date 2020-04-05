71dca46eda71250f52265a39b1cb53bdf0c6c39f
Memo <memovdg@gmail.com>
2020-04-04 19:02:41 -0600

    Conceptos basicos de tailwind pading, margin, tamaños, textos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    extend: {
+      // Agrega directivas
+      fontFamily: {
+        display: ['Gilroy', 'sans-serif'],
+        body: ['Graphik', 'sans-serif']
+      },
+      // Se agregan cosas ademas de las que ya tiene por defecto
+      extend: {
+        colors: {
+          cyan: "#9cbbff"
+        }
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
 }
